2e051eba5e4da4e1efc2f6f3f272984d5ad46102
Julio Casal <julioc@outlook.com>
2018-10-08 08:56:48 -0700

    Add Three Sum

---
+++ Solutions/ThreeSumSolution.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace EPI2018.Solutions
+{
+    public static class ThreeSumSolution
+    {
+        public static bool HasThreeSum(int[] A, int target){
+            Array.Sort(A);
+
+            // a + b + c = t 
+            // a + b = t - c
+            foreach (var element in A)
+            {
+                if (HasTwoSum(A, target - element)){
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private static bool HasTwoSum(int[] A, int target){
+            int i = 0, j = A.Length - 1;
+
+            while (i <= j)
+            {
+                if (A[i] + A[j] > target){
+                    j--;
+                } else if (A[i] + A[j] < target){
+                    i++;
+                } else {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Tests/ThreeSumTests.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using EPI2018.Solutions;
+using Xunit;
+
+namespace EPI2018.Tests
+{
+    public class ThreeSumTests
+    {
+        [Fact]
+        public void ThreeSumExists()
+        {
+            int[] nums = {11, 2, 5, 7, 3};
+            var target = 21;
+            var expected = true;
+            var actual = ThreeSumSolution.HasThreeSum(nums, target);
+            Assert.Equal(expected, actual);
+        }   
+
+        [Fact]
+        public void ThreeSumDoesNotExist()
+        {
+            int[] nums = {11, 2, 5, 7, 3};
+            var target = 22;
+            var expected = false;
+            var actual = ThreeSumSolution.HasThreeSum(nums, target);
+            Assert.Equal(expected, actual);
+        }        
+    }
+}
