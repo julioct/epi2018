be8d64c77a3e37c7cdf19b9cf4a834725b953c20
Julio Casal <julioc@outlook.com>
2018-10-10 07:02:44 -0700

    Add OfflineSampling

---
+++ Solutions/OfflineSamplingSolution.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace EPI2018.Solutions
+{
+    public static class OfflineSamplingSolution
+    {
+        public static void RandomSampling(int k, int[] A){
+            var rand = new Random();
+            for (int i = 0; i < k; i++)
+            {
+                var r = rand.Next(i, A.Length - 1);
+                var t = A[i];
+                A[i] = A[r];
+                A[r] = t;
+            }
+        }
+    }
+}
---
+++ Tests/OfflineSamplingTests.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using EPI2018.Solutions;
+using Xunit;
+
+namespace EPI2018.Tests
+{
+    public class OfflineSamplingTests
+    {
+        [Fact]
+        public void CanRandomSample()
+        {
+            int[] originalArray = {3, 7, 5, 11};
+            var k = 3;
+            var A = originalArray.Clone() as int[];
+
+            OfflineSamplingSolution.RandomSampling(k, A);
+
+            foreach (var element in originalArray)
+            {
+                Assert.Contains(element, A);
+            }
+        }      
+    }
+}
