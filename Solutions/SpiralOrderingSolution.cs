9c278091d41e0d29c3ad990b7b32672338ea2781
Julio Casal <julioc@outlook.com>
2018-10-11 07:22:41 -0700

    Add SpiralOrdering

---
+++ Solutions/SpiralOrderingSolution.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace EPI2018.Solutions
+{
+    public static class SpiralOrderingSolution
+    {
+        public static IList<int> GetSpiralOrder(int[,] matrix){            
+            var len = matrix.GetLength(0);
+            var spiral = new List<int>();
+            var offset = 0;
+
+            while (offset <= len/2){
+                var max = len - 1 - offset;
+
+                if (offset == max){
+                    spiral.Add(matrix[offset, offset]);
+                    return spiral;
+                }
+
+                for(int c = offset; c < max; c++){
+                    spiral.Add(matrix[offset, c]);
+                }
+
+                for (int r = offset; r < max; r++){
+                    spiral.Add(matrix[r, max]);
+                }
+
+                for (int c = max; c > offset; c--){
+                    spiral.Add(matrix[max, c]);
+                }
+
+                for (int r = max; r > offset; r--){
+                    spiral.Add(matrix[r, offset]);
+                }
+
+                offset++;
+            }
+
+            return spiral;
+        }
+    }
+}
---
+++ Tests/SpiralOrderingTests.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using EPI2018.Solutions;
+using Xunit;
+
+namespace EPI2018.Tests
+{
+    public class SpiralOrderingTests
+    {
+        [Fact]
+        public void CanComputeOddSpiral()
+        {
+            var matrix = new int[3,3] { 
+                { 1, 2, 3 }, 
+                { 4, 5, 6 }, 
+                { 7, 8, 9 }, 
+            };
+
+            var expected = new List<int>(new int[]{1, 2, 3, 6, 9, 8, 7, 4, 5});
+            var actual = SpiralOrderingSolution.GetSpiralOrder(matrix);
+
+            for(int i = 0; i < expected.Count; i++)
+            {
+                Assert.Equal(expected[i], actual[i]);
+            }
+        }
+
+        [Fact]
+        public void CanComputeEvenSpiral()
+        {
+            var matrix = new int[4,4] { 
+                {  1,  2,  3,  4 }, 
+                {  5,  6,  7,  8 }, 
+                {  9, 10, 11, 12 }, 
+                { 13, 14, 15, 16 }
+            };
+
+            var expected = new List<int>(new int[]{1, 2, 3, 4, 8, 12, 16, 15, 14, 13, 9, 5, 6, 7, 11, 10});
+            var actual = SpiralOrderingSolution.GetSpiralOrder(matrix);
+
+            for(int i = 0; i < expected.Count; i++)
+            {
+                Assert.Equal(expected[i], actual[i]);
+            }
+        }        
+    }
+}
