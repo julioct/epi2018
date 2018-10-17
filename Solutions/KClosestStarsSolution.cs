a06fa254bd85853a234a97385f69c6021caceaab
Julio Casal <julioc@outlook.com>
2018-10-16 19:11:57 -0700

    Add KClosestStars

---
+++ Solutions/KClosestStarsSolution.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EPI2018.Solutions
+{
+    public static class KClosestStarsSolution
+    {        
+        public static List<Star> FindClosestKStars(int k, IEnumerable<Star> stars){
+            // Should use a Priority Queue but it's not available in .NET
+            var maxHeap = new SortedSet<Star>();
+
+            foreach (var star in stars)
+            {
+                maxHeap.Add(star);
+
+                if (maxHeap.Count > k){
+                    maxHeap.Remove(maxHeap.Max);
+                }
+            }
+
+            return maxHeap.ToList();
+        }
+
+        public class Star : IComparable<Star>{
+            public int X;
+            public int Y;
+            public int Z;
+
+            public Star(int x, int y, int z){
+                X = x;
+                Y = y;
+                Z = z;
+            }
+
+            public double Distance {
+                get {
+                    return Math.Sqrt(X*X + Y*Y + Z*Z);
+                }
+            }
+
+            public int CompareTo(Star other)
+            {
+                return this.Distance.CompareTo(other.Distance);
+            }
+        }
+    }    
+}
---
+++ Tests/KClosestStarsTests.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using EPI2018.Solutions;
+using Xunit;
+
+namespace EPI2018.Tests
+{
+    public class KClosestStarsTests
+    {
+        [Fact]
+        public void CanFindClosestKStars()
+        {
+            var stars = new List<KClosestStarsSolution.Star>();
+            stars.Add(new KClosestStarsSolution.Star(1, 1, 1));
+            stars.Add(new KClosestStarsSolution.Star(25, 25, 25));
+            stars.Add(new KClosestStarsSolution.Star(13, 13, 13));
+            stars.Add(new KClosestStarsSolution.Star(99, 99, 99));
+            stars.Add(new KClosestStarsSolution.Star(5, 5, 5));
+            stars.Add(new KClosestStarsSolution.Star(33, 33, 33));
+            stars.Add(new KClosestStarsSolution.Star(51, 51, 51));
+
+            var k = 3;
+
+            var expected = new List<KClosestStarsSolution.Star>();
+            expected.Add(stars[0]);
+            expected.Add(stars[4]);
+            expected.Add(stars[2]);
+
+            var actual = KClosestStarsSolution.FindClosestKStars(k, stars);
+
+            for (int i = 0; i < expected.Count; i++)
+            {
+                Assert.Equal(expected[i], actual[i]);
+            }
+        }
+    }
+}
