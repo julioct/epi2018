536e4d88002ebd4c36ceb328b609e9e950c6f8e2
Julio Casal <julioc@outlook.com>
2018-10-25 17:21:28 -0700

    Add MaxConcurrentEvents

---
+++ Solutions/MaxConcurrentEventsSolution.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EPI2018.Solutions
+{
+    public static class MaxConcurrentEventsSolution
+    {
+        public static int MaxConcurrentEvents(CalendarEvent[] events){
+            var sorted = events.OrderBy(e => e.End);
+            var maxEventCount = 0;
+            var queue = new Queue<CalendarEvent>();
+
+            foreach(var calendarEvent in events){
+                while(queue.Count != 0 && queue.Peek().End < calendarEvent.Start){
+                    queue.Dequeue();
+                }
+
+                queue.Enqueue(calendarEvent);
+                maxEventCount = Math.Max(maxEventCount, queue.Count);
+            }
+
+            return maxEventCount;
+        }
+
+        public class CalendarEvent{
+            public int Start;
+            public int End;
+
+            public CalendarEvent(int start, int end){
+                Start = start;
+                End = end;
+            }
+        }
+    }    
+}
