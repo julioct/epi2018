c17459ceb93b959e50e8725e8523bbc54feb316b
Julio Casal <julioc@outlook.com>
2018-10-28 10:03:19 -0700

    Add SearchFirstGreaterValueInBst

---
+++ Solutions/SearchFirstGreaterValueInBstSolution.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace EPI2018.Solutions
+{
+    public static class SearchFirstGreaterValueInBstSolution
+    {
+        public static TreeNode<int> FindFirstGreaterThanK(TreeNode<int> tree, int k){
+            TreeNode<int> firstSoFar = null;
+            var subtree = tree;
+
+            while(subtree != null){
+                if (subtree.Data > k){
+                    firstSoFar = subtree;
+                    subtree = subtree.Left;
+                } else {
+                    subtree = subtree.Right;
+                }
+            }
+
+            return firstSoFar;
+        }
+    }    
+}
