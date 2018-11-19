c6b34471afc64e5d93638d1e0bfe50bcc38b1b8d
Julio Casal <julioc@outlook.com>
2018-11-19 06:59:28 -0800

    Add BuyAndSellStock

---
+++ Solutions/BuyAndSellStockSolution.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EPI2018.Solutions
+{
+    public static class BuyAndSellStockSolution
+    {
+        public static double ComputeMaxProfix(double[] prices){
+            var maxProfit = 0.0;
+            var minBuy = double.MaxValue;
+
+            foreach(var price in prices){
+                maxProfit = Math.Max(price - minBuy, maxProfit);
+                minBuy = Math.Min(price, minBuy);
+            }
+
+            return maxProfit;
+        }
+    }    
+}
---
+++ Tests/BuyAndSellStockTests.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using EPI2018.Solutions;
+using Xunit;
+
+namespace EPI2018.Tests
+{
+    public class BuyAndSellStockTests
+    {
+        [Fact]
+        public void CircularQueueBasicTest()
+        {
+            double[] prices = {350,315,275,295,260,270,290,230,255,250};
+
+            var expected = 30;
+            var actual = BuyAndSellStockSolution.ComputeMaxProfix(prices);
+
+            Assert.Equal(expected, actual);
+        }      
+    }
+}
